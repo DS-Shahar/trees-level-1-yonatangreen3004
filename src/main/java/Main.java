f86f766f90c4c5debde40de6c85528a646e3b6e3
yonatangreen3004 <yonatan.green3004@gmail.com>
2026-02-05 09:17:15 +0200

    Implement tree validation method 'ex1'
    Added a method 'ex1' to validate the tree structure based on Range values.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+   public static boolean ex1(TreeNode<Range> t) {
+
+    if (t == null) {
+        return true;
+    }
+
+    Range r = t.getValue();
+    int low = r.getLow();
+    int high = r.getHigh();
+
+    if (t.hasLeft()) {
+        Range rLeft = t.getLeft().getValue();
+        int lowLeft = rLeft.getLow();
+        int highLeft = rLeft.getHigh();
+
+        if (low <= lowLeft && high >= highLeft) {
+            return false;
+        }
+    }
+
+    if (t.hasRight()) {
+        Range rRight = t.getRight().getValue();
+        int lowRight = rRight.getLow();
+        int highRight = rRight.getHigh();
+
+        if (highRight == high && lowRight > low) {
+            return false;
+        }
+    }
+
+    if (t.hasRight() && t.hasLeft()) {
+        if (t.getLeft().getValue().getHigh() >=
+            t.getRight().getValue().getLow()) {
+            return false;
+        }
+    }
+
+    return ex1(t.getLeft()) && ex1(t.getRight());
+}
   }
 }
